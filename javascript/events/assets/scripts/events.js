9bbd34161c2afc6488f66b9ca1c29ddac08a7abc
morozov-dima <md0545483478@gmail.com>
2021-12-27 08:24:35 +0200

    events

---
+++ javascript/events/assets/scripts/events.js
@@ -0,0 +1,109 @@
+const button = document.querySelector('button');
+
+// button.onclick = function() {
+
+// };
+
+
+const buttonClickHandler  = event => {
+    // event.target.disabled = true;
+    console.log(event);
+};
+
+
+
+const anotherButtonClickHandler = () => {
+    console.log('This was clicked!');
+};
+
+
+// button.onclick = buttonClickHandler;
+// button.onclick = anotherButtonClickHandler;
+
+
+// buttons.forEach(btn => {
+//     btn.addEventListener('mouseenter', buttonClickHandler);
+// });
+
+
+// window.addEventListener('scroll', event => {
+//     console.log(event);
+// });
+
+
+// button.addEventListener('click', buttonClickHandler);
+
+// setTimeout(() => {
+//     button.removeEventListener('click', buttonClickHandler);
+// }, 2000);
+
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log(event);
+});
+
+
+
+const div = document.querySelector('div');
+
+
+
+
+
+
+div.addEventListener('click', event => {
+    console.log('clicked div');
+    console.log(event);
+}); // set to capturing (default is false)
+
+
+// div.addEventListener('click', event => {
+//     console.log('clicked div');
+//     console.log(event);
+// }, true); // set to capturing (default is false)
+
+
+
+
+
+
+button.addEventListener('click', event => {
+    event.stopPropagation();
+    console.log('ckicked button');
+    console.log(event); 
+});
+
+
+
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+
+
+// listItems.forEach(listItem => {
+//     listItem.addEventListener('click', event => {
+//         console.log(event);
+//         event.target.classList.toggle('highlight');
+//     });
+// });
+
+
+
+
+list.addEventListener('click', event => {
+    event.target.classList.toggle('highlight');
+});
+
+
+
+
+
+
+
+
+
+
