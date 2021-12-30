417d9ef403dce28d761056585ad8676c9fde512a
morozov-dima <md0545483478@gmail.com>
2021-12-30 19:37:14 +0200

    xhr

--- javascript/http/assets/scripts/app.js
+++ javascript/http/assets/scripts/app.js
@@ -41,8 +41,11 @@
 */
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
 
-function sendHttpRequest(method, url){
+
+function sendHttpRequest(method, url, data){
     const promise = new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.open(method, url);
@@ -50,7 +53,7 @@ function sendHttpRequest(method, url){
         xhr.onload = function() {
             resolve(xhr.response);
         }
-        xhr.send();
+        xhr.send(JSON.stringify(data));
     });
 
     return promise;
@@ -58,39 +61,69 @@ function sendHttpRequest(method, url){
 
 
 // option 1 (with then)
-function fetchPosts() {
-    sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
-    .then((responseData) => {
-            //const listOfPosts = JSON.parse(xhr.response);
-            const listOfPosts = responseData;   // if we add "xhr.responseType = 'json';" we can not use "JSON.parse "
-            for (const post of listOfPosts) {
-                 const postEl = document.importNode(postTemplate.content, true);    // importNode() method creates a copy of a Node
-                 postEl.querySelector('h2').textContent = post.title.toUpperCase();
-                 postEl.querySelector('p').textContent = post.body;
-                 listElement.append(postEl);
-            }
-    });
-}
-
-
-
-// option 2 (with async await)
-// async function fetchPosts() {
-//     const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
-//         //const listOfPosts = JSON.parse(xhr.response);
-//         const listOfPosts = responseData;   // if we add "xhr.responseType = 'json';" we can not use "JSON.parse "
-//         for (const post of listOfPosts) {
-//                 const postEl = document.importNode(postTemplate.content, true);    // importNode() method creates a copy of a Node
-//                 postEl.querySelector('h2').textContent = post.title.toUpperCase();
-//                 postEl.querySelector('p').textContent = post.body;
-//                 listElement.append(postEl);
-//         }
+// get data from server
+// function fetchPosts() {
+//     sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
+//     .then((responseData) => {
+//             //const listOfPosts = JSON.parse(xhr.response);
+//             const listOfPosts = responseData;   // if we add "xhr.responseType = 'json';" we can not use "JSON.parse "
+//             for (const post of listOfPosts) {
+//                  const postEl = document.importNode(postTemplate.content, true);    // importNode() method creates a copy of a Node
+//                  postEl.querySelector('h2').textContent = post.title.toUpperCase();
+//                  postEl.querySelector('p').textContent = post.body;
+//                  listElement.append(postEl);
+//             }
+//     });
 // }
 
 
 
+// option 2 (with async await)
+// get data from server
+async function fetchPosts() {
+    console.log('fetchPosts');
+    const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+        //const listOfPosts = JSON.parse(xhr.response);
+        const listOfPosts = responseData;   // if we add "xhr.responseType = 'json';" we can not use "JSON.parse "
+        for (const post of listOfPosts) {
+                const postEl = document.importNode(postTemplate.content, true);    // importNode() method creates a copy of a Node
+                postEl.querySelector('h2').textContent = post.title.toUpperCase();
+                postEl.querySelector('p').textContent = post.body;
+                
+                listElement.append(postEl);
+        }
+}
 
-fetchPosts();
+
+// send data to server
+async function createPost(title, content) {
+     const userId = Math.random();
+     const post = {
+       title: title,
+       body: content,
+       userId: userId 
+     };
+
+     sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+}
+
+
+
+fetchButton.addEventListener('click', fetchPosts);
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const enteredTitle = event.currentTarget.querySelector('#title').value;
+    const enteredContent = event.currentTarget.querySelector('#content').value;
+    createPost(enteredTitle, enteredContent);
+
+
+});
+
+createPost('DUMMY', 'A dummy post!');
+
+
+
+
 
 
 
