e756ed437a658e8396776cc35db27a38ade4caba
morozov-dima <md0545483478@gmail.com>
2022-01-01 10:18:39 +0200

    fetch API

--- javascript/http/assets/scripts/app.js
+++ javascript/http/assets/scripts/app.js
@@ -1,12 +1,10 @@
 /*
-    In this module we will use https://jsonplaceholder.typicode.com
+In this module we will use https://jsonplaceholder.typicode.com
 */
 
 
-
-
 /* 
-    Example 1
+Example 1
 */
 // const listElement = document.querySelector('.posts');
 // const postTemplate = document.getElementById('single-post');
@@ -14,85 +12,131 @@
 // xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 // xhr.responseType = 'json';
 // xhr.onload = function() {
-//     //const listOfPosts = JSON.parse(xhr.response);
-//     const listOfPosts = xhr.response;   // if we add "xhr.responseType = 'json';" we can not use "JSON.parse "
-//     //console.log(listOfPosts);
-//     for (const post of listOfPosts) {
-//          const postEl = document.importNode(postTemplate.content, true);    // importNode() method creates a copy of a Node 
-//          postEl.querySelector('h2').textContent = post.title.toUpperCase();
-//          postEl.querySelector('p').textContent = post.body;
-//          listElement.append(postEl);
-//     }
-// }
-// xhr.send();
+    //     //const listOfPosts = JSON.parse(xhr.response);
+    //     const listOfPosts = xhr.response;   // if we add "xhr.responseType = 'json';" we can not use "JSON.parse "
+    //     //console.log(listOfPosts);
+    //     for (const post of listOfPosts) {
+        //          const postEl = document.importNode(postTemplate.content, true);    // importNode() method creates a copy of a Node 
+        //          postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        //          postEl.querySelector('p').textContent = post.body;
+        //          listElement.append(postEl);
+        //     }
+        // }
+        // xhr.send();
+        
+        
 
 
 
 
+        
+        
+    /* 
+        Example 2 (with promises + XMLHttpRequest)
+    */
+    // const listElement = document.querySelector('.posts');
+    // const postTemplate = document.getElementById('single-post');
+    // const form = document.querySelector('#new-post form');
+    // const fetchButton = document.querySelector('#available-posts button');
+    // const postList = document.querySelector('ul');
+    
+    
+    // function sendHttpRequest(method, url, data){
+    //     const promise = new Promise((resolve, reject) => {
+            //  const xhr = new XMLHttpRequest();
+            //  xhr.setRequestHeader('Content-Type', 'application/json');
+    //         xhr.open(method, url);
+    //         xhr.responseType = 'json';
+    //         xhr.onload = function() {
+    //             if(xhr.status >= 200 && xhr.status < 300) {
+    //                 resolve(xhr.response);
+    //             }else {
+    //                 reject(new Error('something went wrong!!!'));
+    //             }
+    //         }
+                
+    //         xhr.onerror = function() {
+    //            reject(new Error('Failed to send request!'));
+    //         }
+    
+    //         xhr.send(JSON.stringify(data));
+    //     });
+    
+    //     return promise;
+    // }
+    
+    
+    
+    
+    
+    
+    // option 1 (with then)
+    // get data from server
+    // function fetchPosts() {
+    //     sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
+    //     .then((responseData) => {
+    //             //const listOfPosts = JSON.parse(xhr.response);
+    //             const listOfPosts = responseData;   // if we add "xhr.responseType = 'json';" we can not use "JSON.parse "
+    //             for (const post of listOfPosts) {
+    //                  const postEl = document.importNode(postTemplate.content, true);    // importNode() method creates a copy of a Node
+    //                  postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    //                  postEl.querySelector('p').textContent = post.body;
+    //                  listElement.append(postEl);
+    //             }
+    //     });
+    // }
+    
+    
+    
+        
+        
+        
+        
+    /* 
+    Example 2 (fetch API)
+    */
+    const listElement = document.querySelector('.posts');
+    const postTemplate = document.getElementById('single-post');
+    const form = document.querySelector('#new-post form');
+    const fetchButton = document.querySelector('#available-posts button');
+    const postList = document.querySelector('ul');
 
-
-
-
-
-
-
-/* 
-    Example 2 (with promises)
-*/
-const listElement = document.querySelector('.posts');
-const postTemplate = document.getElementById('single-post');
-const form = document.querySelector('#new-post form');
-const fetchButton = document.querySelector('#available-posts button');
-const postList = document.querySelector('ul');
-
-
-function sendHttpRequest(method, url, data){
-    const promise = new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.responseType = 'json';
-        xhr.onload = function() {
-            if(xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.response);
-            }else {
-                reject(new Error('something went wrong!!!'));
+    function sendHttpRequest(method, url, data){
+        return fetch(url, {
+            method:  method,
+            body: JSON.stringify(data),
+            headers: {
+            'Content-Type': 'application/json' 
             }
-        }
-         
-        xhr.onerror = function() {
-           reject(new Error('Failed to send request!'));
-        }
+        }).then(response => {
+            if (response.status >= 200 && response.status < 300) { // success case
+                return response.json(); 
+            } else {
+                throw new Error('Something went wrong - server-side.');
+            }
+        }).catch(error => {
+            console.log(error);
+            throw new Error('Something went wrong!');
+        }); 
 
-        xhr.send(JSON.stringify(data));
-    });
-
-    return promise;
-}
+    }
 
 
-// option 1 (with then)
-// get data from server
-// function fetchPosts() {
-//     sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
-//     .then((responseData) => {
-//             //const listOfPosts = JSON.parse(xhr.response);
-//             const listOfPosts = responseData;   // if we add "xhr.responseType = 'json';" we can not use "JSON.parse "
-//             for (const post of listOfPosts) {
-//                  const postEl = document.importNode(postTemplate.content, true);    // importNode() method creates a copy of a Node
-//                  postEl.querySelector('h2').textContent = post.title.toUpperCase();
-//                  postEl.querySelector('p').textContent = post.body;
-//                  listElement.append(postEl);
-//             }
-//     });
-// }
+
+
+
+
+
+
+
 
 
 
 // option 2 (with async await)
 // get data from server
 async function fetchPosts() {
-    try {
-        const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+    // try {
+        const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/postsXXX');
             //const listOfPosts = JSON.parse(xhr.response);
             const listOfPosts = responseData;   // if we add "xhr.responseType = 'json';" we can not use "JSON.parse "
             for (const post of listOfPosts) {
@@ -102,9 +146,9 @@ async function fetchPosts() {
                     postEl.querySelector('li').id = post.id;                
                     listElement.append(postEl);
             }
-    } catch (error) {
-        console.error("Our error is : " + error.message);
-    }
+    // } catch (error) {
+    //     console.error("Our error is : " + error.message);
+    // }
 }
 
 
