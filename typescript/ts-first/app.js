be35ecbbedd4420b86c8fc21665ae8615ac0db58
morozov-dima <md0545483478@gmail.com>
2022-01-05 08:04:01 +0200

    typeScipt

--- typescript/ts-first/app.js
+++ typescript/ts-first/app.js
@@ -1,23 +1,44 @@
 /*
   run "tsc app.ts" in order to convers "ts" code to "js" code
 */
+var User = /** @class */ (function () {
+    function User(name, age) {
+        this.name = name + '1';
+        this.age = age;
+    }
+    return User;
+}());
+var user = new User('Max', 30);
+console.log(user.name);
 var num1Input = document.getElementById('num1');
 var num2Input = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
 function add(a, b) {
     return a + b;
 }
-function printResult(result) {
-    console.log(result);
+//enum OutputMode { CONSOLE, ALERT };
+function printResult(result, printMode) {
+    if (printMode === 'console') {
+        console.log(result);
+    }
+    else if (printMode === 'alert') {
+        alert(result);
+    }
 }
-// const result = add(5, 3);
-// const isDone = false;
-// console.log(result);
-// printResult("Skoda");
-// printResult(12);
+var results = [];
+var names = ['Max'];
 buttonElement.addEventListener('click', function () {
-    var num1 = num1Input.value;
-    var num2 = num2Input.value;
+    var num1 = +num1Input.value; // we add + to convert string info a number
+    var num2 = +num2Input.value; // we add + to convert string info a number
     var result = add(num1, num2);
-    printResult(result);
+    var resultContainer = {
+        res: result,
+        print: function () {
+            console.log(this.res);
+        }
+    };
+    results.push(resultContainer);
+    // results[0].print();
+    printResult(result, 'console');
+    printResult(result, 'alert');
 });
--- typescript/ts-first/app.ts
+++ typescript/ts-first/app.ts
@@ -2,6 +2,44 @@
   run "tsc app.ts" in order to convers "ts" code to "js" code
 */
 
+
+class User {
+  constructor(
+    public name: string,
+    private age: number
+  ){
+
+
+  }
+}
+
+
+class Admin extends User {
+  constructor(
+    name: string,
+    age: number,
+    private permissions: string[]
+  ){
+    // we will forward name and age to the super consrtuctor (to the User class)
+    super(name, age);
+  }
+}
+
+
+
+const user = new User('Max', 30 );
+console.log(user.name);
+
+
+
+
+
+
+
+
+
+
+
 const num1Input = document.getElementById('num1') as HTMLInputElement;
 const num2Input = <HTMLInputElement>document.getElementById('num2');
 const buttonElement = document.querySelector('button');
@@ -10,8 +48,16 @@ function add(a: number, b: number) {
   return a + b;
 }
 
-function printResult(result) {
-  console.log(result);
+type PrintMode = 'console' | 'alert';
+//enum OutputMode { CONSOLE, ALERT };
+
+function printResult(result: string | number, printMode: PrintMode) {
+  if (printMode === 'console') {
+    console.log(result);
+  } else if (printMode === 'alert') {
+    alert(result);
+  }
+
 }
 
 // const result = add(5, 3);
@@ -22,15 +68,24 @@ function printResult(result) {
 // printResult("Skoda");
 // printResult(12);
 
+type CalculationResults = { res: number, print: () => void }[];
 
+const results: CalculationResults = [];
+const names = ['Max'];
 
 buttonElement.addEventListener('click', () => {
   const num1 = +num1Input.value; // we add + to convert string info a number
   const num2 = +num2Input.value; // we add + to convert string info a number
   const result = add(num1, num2);
-  const resultContainer: { res: number } = {
-    res: result
+  const resultContainer = {
+    res: result,
+    print() {
+      console.log(this.res);
+    }
   };
-  printResult(resultContainer.res);
+  results.push(resultContainer);
+  // results[0].print();
+  printResult(result, 'console');
+  printResult(result, 'alert');
 });
 
