c40e3df765b9d6521635c226bc0a97869ee6cb4c
morozov-dima <md0545483478@gmail.com>
2022-01-05 10:59:01 +0200

    union types

--- typescript/union-types/app.ts
+++ typescript/union-types/app.ts
@@ -3,13 +3,22 @@ function combine(input1: number | string, input2: number | string) {
   if (typeof input1 === 'number' && typeof input2 === 'number') {
     result = input1 + input2;
   } else {
-    result = input1.toString() + input2.toString();
+    result = input1.toString() + input2.toString();   /* convert number to string */
   }
   return result;
 }
 
+
 const combinedAges = combine(30, 26);
-console.log(combinedAges);
+console.log(combinedAges);   /* output result of combine numbers */
+
 
 const combinedNames = combine('Max', 'Anna');
-console.log(combinedNames);
+console.log(combinedNames);   /* output result of combine strings */
+
+
+
+
+
+
+
