7351262202fbdc1bbf339d3c6de13d3bcb5149e0
morozov-dima <md0545483478@gmail.com>
2022-01-06 14:57:35 +0200

    js es6

--- typescript/config/src/analytics.ts
+++ typescript/config/src/analytics.ts
@@ -1,2 +1,10 @@
-console.log('Sending...');
-console.log('Sending...');
+let logged;
+
+function sendAnalytics(data: string) {
+    console.log(data);
+    logged = true;
+    
+}
+
+
+sendAnalytics('The data');
