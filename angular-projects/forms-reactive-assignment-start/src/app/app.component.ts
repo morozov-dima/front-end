3f25ffb5ae599f4be26c4131cee0a327e6ff7c33
morozov-dima <md0545483478@gmail.com>
2022-02-14 11:35:33 +0200

    reactive forms

--- angular-projects/forms-reactive-assignment-start/src/app/app.component.ts
+++ angular-projects/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs/Observable';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
@@ -10,57 +10,38 @@ import { Observable } from 'rxjs/Observable';
 export class AppComponent implements OnInit {
   profileForm: FormGroup;
 
+  // list of project status for dropdown list
   projectStatus: string[] = ['Stable','Critical','Finished'];
-  forbiddenProjectNames: string[] = ['Test'];
+
 
   ngOnInit(): void {
+    /* 
+      Reactive form approach.
+    */
     this.profileForm = new FormGroup({
-      projectName: new FormControl('', [Validators.required, this.forbiddenProjectName.bind(this)]),
-      projectEmail: new FormControl('', [Validators.required, Validators.email]),
-      projectEmailAsync: new FormControl('', [Validators.required, Validators.email], this.forbiddenProjectEmailsAsync),
-      projectStatus: new FormControl('')
+      'projectName': new FormControl('', [Validators.required, CustomValidators.forbiddenProjectName.bind(this)]),
+      'projectEmail': new FormControl('', [Validators.required, Validators.email]),
+      'projectEmailAsync': new FormControl('', [Validators.required, Validators.email], CustomValidators.forbiddenProjectEmailsAsync.bind(this)),
+      
+       // we set default value for our dropdown element 
+      'projectStatus': new FormControl('Critical')
     });
   }
 
+
+
+
+
+
+  /*
+    Submit button in our form
+  */
   onSubmit() {
     console.log(this.profileForm);
     //this.profileForm.reset();
   }
 
 
-
-  forbiddenProjectName(control: FormControl): {[s: string]: boolean} {
-    // check if 'projectStatus' array contains the value of our control
-    if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
-      return {'projecNameIsForbidden': true}
-    }
-    // if validation is successful, you have to pass nothing or null.
-    return null;
-  }
-
-
-
-
-
-  forbiddenProjectEmailsAsync(control: FormControl): Promise<any> | Observable<any> {
-    const forbiddenProjectEmails: string[] = ['test@test123.com'];
-    const promise = new Promise<any>(
-      (resolve, reject) => {
-
-        // we want right now to simulate the fact that we have
-        // an asynchronous task like reaching out to a server.
-        // for this reason we add setTimeout
-        setTimeout(() => {
-          if (forbiddenProjectEmails.indexOf(control.value) !== -1) {
-            resolve({'projecEmailsIsForbiddenAsync': true})
-          } else{
-            resolve(null)
-          }
-        }, 1500);
-      }
-    );
-    return promise;
-  }
 
 
   
