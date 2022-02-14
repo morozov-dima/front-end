64ae926253a835071f23d5249e509251525429df
morozov-dima <md0545483478@gmail.com>
2022-02-14 18:17:03 +0200

    reactive forms

--- angular-projects/form-reactive-pr1/src/app/contact-us/contact-us.component.ts
+++ angular-projects/form-reactive-pr1/src/app/contact-us/contact-us.component.ts
@@ -7,21 +7,52 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  
+  // access our form
   heroForm!: FormGroup;
 
+  userName: string = '';
+  userEmail: string = '';
+  userSkils: string = '';
+  userComments: string = '';
+
   ngOnInit(): void {
+
+    // set form controls
     this.heroForm = new FormGroup({  
-      nameInput: new FormControl(null, Validators.required),
-      emailInput: new FormControl(null, [Validators.required, Validators.email]),
-      userComments: new FormControl(null, Validators.required)
+      'nameInput': new FormControl(null, Validators.required),
+      'emailInput': new FormControl(null, [Validators.required, Validators.email]),
+      'userSkils': new FormControl(null),
+      'userComments': new FormControl(null, Validators.required)
     }); 
   }
 
 
 
+
+
+
+
+  /*
+    Run this function  when form is submitted
+  */
   onSumbit() {
       console.log(this.heroForm);
+
+      // get field values from submitted form
+      this.userName = this.heroForm.get('nameInput')!.value;
+      this.userEmail = this.heroForm.get('emailInput')!.value;
+      this.userSkils = this.heroForm.get('userSkils')!.value;
+      this.userComments = this.heroForm.get('userComments')!.value;
       
+      // reset form after form submitted
+      this.heroForm.reset();
   }
+
+
+
+
+
+
 
 }
