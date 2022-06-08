81030b72bc17d4dd24e778113b3ce7a5bd6df06b
morozov-dima <md0545483478@gmail.com>
2022-06-08 14:40:47 +0300

    file updates

--- reference/ANGULAR-FORMS/REACTIVE-FORMS/custom-validators/custom-validators.ts
+++ reference/ANGULAR-FORMS/REACTIVE-FORMS/custom-validators/custom-validators.ts
@@ -4,35 +4,66 @@
 
 
 
-// ************************** contact-us.component.ts *****************************
-import { Component, OnInit } from '@angular/core';
+
+
+// ****************************** app.component.ts ********************************
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { CustomValidator } from '../shared/custom-validator';
+import { Subscription } from 'rxjs';
+import { forbiddenNameValidator } from './shared/custom-form-validator';
+import { FormData } from './shared/form-data.interface';
+import { FormDataService } from './shared/form-data.service';
 
 @Component({
-  selector: 'app-contact-us',
-  templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class ContactUsComponent implements OnInit {
-  profileForm!: FormGroup;
-  constructor() { }
+export class AppComponent {
 
-  ngOnInit(): void {
-    this.profileForm = new FormGroup({
-      'fullNameInput': new FormControl('', Validators.required),
-      'emailNameInput': new FormControl('', [ Validators.required, Validators.email ], CustomValidator.forbiddenEmails)
-    });
-  }
+  userForm = new FormGroup({
+    formEmail: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      /*
+       * Here's how you pass in the custom validator.
+       * you can pass value ro regular expression '/morozov/i'
+      */
+      forbiddenNameValidator(/morozov/i)  // <-- custom validator.
+    ]),
+    formPass: new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
+
+  constructor(private formDataService: FormDataService) {}
+
+  isSubmitted: boolean = false;
+  formDataSub!: Subscription;
+  formReferenseId!: number;
 
   onSubmit() {
-    console.log(this.profileForm);
-    this.profileForm.reset();
+    const formValue: FormData = {
+      email: this.userForm.value.formEmail,
+      passowrd: this.userForm.value.formPass
+    };
+    this.formDataSub = this.formDataService.submitformData(formValue).subscribe({
+      next: (response) => {
+        this.userForm.reset();
+        this.isSubmitted = true;
+        if (response.id) {
+          this.formReferenseId = response.id;
+        }
+      }
+    })
   }
 
-  // we can always access any form control through the get method
-  get fullNameInput() { return this.profileForm.get('fullNameInput')!;  }
-  get emailNameInput() { return this.profileForm.get('emailNameInput')!; }
+  get formEmail() {
+    return this.userForm.get('formEmail')!;
+  }
+
+  get formPass() {
+    return this.userForm.get('formPass')!;
+  }
+
 }
 
 
@@ -43,29 +74,80 @@ export class ContactUsComponent implements OnInit {
 
 
 
-// *************************** custom-validator.ts ****************************
-import { AbstractControl } from "@angular/forms";
-import { Observable } from "rxjs";
+// *************************** custom-form-validator.ts ****************************
 
-export class CustomValidator {
-    static forbiddenEmails(control: AbstractControl): Promise<any> | Observable<any> {
-        const promise = new Promise((resolve, reject) => {
-          // we simulate the fact that we have an asynchronous task like reaching out to a server.
-          // for this reason we add setTimeout
-          setTimeout(() => {
-              if(control.value === 'test@test.com') {
-                // if validation is false
-                resolve({'emailIsForbidden': true})
-              } else {
-                  // if validation is successful , you have to pass nothing or null.
-                  resolve(null)
-              }
-          }, 1500);
-        });
-        return promise;
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+        const forbidden = nameRe.test(control.value);
+        return forbidden ? {forbiddenName: {value: control.value}} : null;
+    };
+}
+
+
+
+
+
+
+
+// ************************** form-data.interface.ts *****************************
+export interface FormResponse {
+  email: string;
+  referenseId: number;
+  id?: number;
+}
+
+export interface FormData {
+  email: string;
+  passowrd: string;
+}
+
+
+
+
+
+
+
+
+// *************************** form-data.service.ts ******************************
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { FormResponse, FormData } from './form-data.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormDataService {
+
+  constructor(private http: HttpClient) { }
+
+  submitformData(formData: FormData): Observable<FormResponse> {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    return this.http.post<FormResponse>(url, formData, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
     }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
 }
-
-
-
-
